cc8eaa6cee147a476da8546a2a494cf2d609d0da
Parth_Nath_Chauhan <parth26nath@gmail.com>
2024-12-18 01:57:20 +0530

    main

--- dex/src/components/Swap.js
+++ dex/src/components/Swap.js
@@ -85,7 +85,7 @@ function Swap(props) {
 
   async function fetchPrices(one, two){
 
-      const res = await axios.get(`http://localhost:3001/tokenPrice`, {
+      const res = await axios.get(`trustdex-backend.railway.internal/tokenPrice`, {
         params: {addressOne: one, addressTwo: two}
       })
 
